ebf7518a6fad86f5d1f56dae8ed59c514de63670
Daniëlle <Danielle@MacBook-Air-van-Danielle-2.local>
2021-03-15 16:10:38 +0100

    d

---
+++ www/init.R
@@ -0,0 +1,44 @@
+# Initialization ----
+# Sets up project environment.
+rm(list = ls())
+
+packages = c(
+  "prob",
+  "data.table",
+  "distrEx",
+  "LaplacesDemon",
+  "formattable",
+  "kableExtra",
+  "knitr",
+  "TeachingDemos",
+  "dplyr",
+  "dbplyr",
+  "tidyverse",
+  "Hmisc",
+  "psych",
+  "samplingbook",
+  "swirl",
+  "ggplot2",
+  "swirl",
+  "snpar",
+  "BSDA",
+  "actuar",
+  "readxl",
+  "stargazer",
+  "jtools",
+  "semPlot",
+  "lavaan",
+  "hrbrthemes",
+  "gridExtra"
+)
+
+invisible(
+  lapply( # For each element in...
+    packages, # ... the packages vector (iterable) ...
+    function(i) { # ... apply this function ...
+      library(i, character.only = TRUE) # load the package
+    }
+  )
+)
+
+author_name = Sys.getenv("USER")
